f8e7a123b271c2f2b60359accec1435ff898a22e
Bonvic bundi <nyabuyabonvic@gmail.com>
2019-03-28 09:28:31 +0300

    [feature #164950025] created a relationship to food

--- api/app/Model/Cart.php
+++ api/app/Model/Cart.php
@@ -2,9 +2,13 @@
 
 namespace App\Model;
 
+use App\Model\Food;
 use Illuminate\Database\Eloquent\Model;
 
 class Cart extends Model
 {
-    //
+    public function foods(){
+
+        return $this->hasMany(Food::class);
+    }
 }
