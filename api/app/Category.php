afec8b4a0d34fd2c2e56dc985d6ccc88a7dd0081
Bonvic bundi <nyabuyabonvic@gmail.com>
2019-03-30 13:22:12 +0300

    [feature #165000324] created relationship

--- api/app/Category.php
+++ api/app/Category.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App;
+use App\Food;
 
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,4 +11,9 @@ class Category extends Model
         'name',
         'description'
     ];
+
+    public function foods()
+    {
+        return $this->hasMany(Food::class);
+    }
 }
