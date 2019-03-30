0047d0ddd6ddf9eb5be8d006529043f66d950cc9
Bonvic bundi <nyabuyabonvic@gmail.com>
2019-03-30 23:56:45 +0300

    [feature #165000324]adds reviw resource

---
+++ api/app/Http/Resources/review/ReviewResource.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Resources\review;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReviewResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return parent::toArray($request);
+    }
+}
