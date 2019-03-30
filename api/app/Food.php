980bb599d2478a024a5b96a4815a292f44381405
Bonvic bundi <nyabuyabonvic@gmail.com>
2019-03-30 23:39:15 +0300

    [feature #165000324]build function ratings

--- api/app/Food.php
+++ api/app/Food.php
@@ -5,6 +5,7 @@ namespace App;
 use App\Order;
 use App\Reviews;
 use App\Category;
+use App\Http\Resources\review\ReviewCollection;
 use Illuminate\Database\Eloquent\Model;
 
 class Food extends Model
@@ -30,5 +31,16 @@ class Food extends Model
     public function category()
     {
         return $this->belongsTo(Category::class,'category_id');
+    }
+    public function getReview()
+    {
+       return $this->reviews->count()>0 ? round($this->reviews->sum('star')/ $this->reviews->count(),1): 0;
+    }
+    public function getFoods($id)
+    {
+        return  new ReviewCollection($this::find($id)->reviews);
+    }
+    public function sumTotal($discount, $price){
+        return ((1-($discount/100))*$price);
     }
 }
