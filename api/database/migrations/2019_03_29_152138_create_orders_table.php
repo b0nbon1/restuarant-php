b31d80acac5cd6b9a58a5ced2ef3d4906e3461f9
Bonvic bundi <nyabuyabonvic@gmail.com>
2019-03-30 07:43:49 +0300

    [feature #164982777]migration for orders

---
+++ api/database/migrations/2019_03_29_152138_create_orders_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('quantity');
+            $table->timestamps();
+
+            $table->integer('food_id')->unsigned()->index();
+            $table->foreign('food_id')->references('id')->on('foods')->onDelete('cascade');
+            $table->integer('user_id')->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('foods')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
