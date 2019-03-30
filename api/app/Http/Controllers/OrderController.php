23af79d4379f7fd51bccc9dfa3712d3cacd0a1e8
Bonvic bundi <nyabuyabonvic@gmail.com>
2019-03-30 07:43:09 +0300

    [feature #164982777] create new order controller

---
+++ api/app/Http/Controllers/OrderController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $order = Order::all();
+
+        return response()->json([$order],200);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'quantity' => 'required|numeric',
+            'food_id'=>'required|exists:foods,id',
+            'user_id'=>'nullable|exists:users,id'
+        ]);
+
+        $order = Order::create($request->all());
+
+        return response()->json([
+            'message' => 'Order Created Successful',
+            'food' => $order
+        ], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Order $order)
+    {
+        return $order;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Order $order)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Order $order)
+    {
+        $request->validate([
+            'quantity' => 'nullable|numeric',
+            'food_id'=>'nullable|exists:foods,id',
+            'user_id'=>'nullable|exists:users,id'
+        ]);
+
+        $order->update($request->all());
+ 
+         return response()->json([
+             'message' => 'order updated',
+             'task' => $order
+         ],201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Order  $order
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Order $order)
+    {
+        $order->delete();
+
+        return response()->json([
+            'message' => 'Successfully deleted order!'
+        ]);
+    }
+}
