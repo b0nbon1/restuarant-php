4b0eff90006d10bbf1597d910f7f20efc6fd4d84
Bonvic bundi <nyabuyabonvic@gmail.com>
2019-04-02 12:47:54 +0300

    [feature #164984245]Created db profile pics

---
+++ api/database/migrations/2019_04_01_133451_create_profile_pics_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilePicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile_pics', function (Blueprint $table) {
+            $table->string("path");
+            $table->timestamps();
+
+            $table->integer("user_id")->unsigned()->index();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile_pics');
+    }
+}
