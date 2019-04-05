6b792d222b9e46e9428f86daa51e86f65919d1fb
Bonvic bundi <nyabuyabonvic@gmail.com>
2019-04-05 10:55:23 +0300

    [feature #165118226]update table orders

--- api/database/migrations/2019_03_29_152138_create_orders_table.php
+++ api/database/migrations/2019_03_29_152138_create_orders_table.php
@@ -16,7 +16,7 @@ class CreateOrdersTable extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
             $table->string('quantity');
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->decimal('total_price');
             $table->timestamps();
 
