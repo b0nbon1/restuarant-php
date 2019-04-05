7523453ed4d48820ae4303cb4efb4972843487e7
Bonvic bundi <nyabuyabonvic@gmail.com>
2019-04-05 10:51:02 +0300

    [feature #165118226] error handler for model exception

--- api/app/Exceptions/Handler.php
+++ api/app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
@@ -46,6 +47,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof ModelNotFoundException) {
+            return response()->json([
+                'error' => 'Entry for '.str_replace('App\\', '', $exception->getModel()).' not found'], 404);
+        }
+
         return parent::render($request, $exception);
     }
 }
